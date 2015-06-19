b4c50cb5f5bfd5257be651fd4d15c41aa84bcea7
Vishnu Anand <vishnu@codemazk.com>
2015-06-19 23:04:11 +0530

    Create classie.js
    Classie javascript file and defining classes

---
+++ js/classie.js
@@ -0,0 +1,46 @@
+( function( window ) {
+'use strict';
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+) + className + "(\\s+|$)");
+}
+
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+if ( typeof define === 'function' && define.amd ) {
+    define( classie );
+} else {
+  window.classie = classie;
+}
+
+})( window );
